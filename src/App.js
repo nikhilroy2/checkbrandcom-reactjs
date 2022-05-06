ec133207191e2afb4169ad4c65c8b6e9dc56eac3
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-05-06 20:44:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Base from "./Layout/Base/Base";
 
+// .....................Pages..................
+import MainPage from "./Pages/MainPage/MainPage";
+// .....................Pages..................End
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Base>
+        <MainPage></MainPage>
+      </Base>
     </div>
   );
 }
