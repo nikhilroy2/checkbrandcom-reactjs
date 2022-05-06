8e731ea1b75bb81c747cb43c0e802c37cc19ff32
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-05-06 21:41:14 +0600

    preparing to start header

--- README.md
+++ README.md
@@ -4,3 +4,33 @@
 <a href="https://checkbrandcom-reactjs.netlify.app/">
     https://checkbrandcom-reactjs.netlify.app/
 </a>
+
+
+
+<!-- Project goal -->
+
+.......................Pages & Components...................
+1st day:
+1. Main page
+2. Explore
+
+
+2nd day:
+3. Account Collection == 2 tabs: Items and Activity
+
+
+3rd & 4th day:
+
+4. User profile with tabs, tables etc.
+5. Stats
+
+5th day:
+6. All categories, Bonds, Options - Collection name, Pack name == with filter, Search, select functional
+
+
+6th & 7th day:
+7. Account / Collection / Pack / Token Creating
+
+8th day:
+9. Control panel
+10. All Filters
--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import React from "react";
 
 function Header(props) {
-    return (
-        <header></header>
-    );
+  return (
+    <header>
+      <nav>
+        <div className="logo_wrapper">
+          <a href="#">Logo</a>
+        </div>
+        <div className="searchBox_wrapper">
+          <input type="text" className="form-control" />
+        </div>
+        <div className="nav_list_wrapper">
+          <ul className="list-unstyled">
+            <li>
+              <a href="#">Explore</a>
+            </li>
+
+            <li>
+              <a href="#">Stats</a>
+            </li>
+
+            <li>
+              <a href="#">Resources</a>
+            </li>
+
+            <li>
+              <a href="#">Create</a>
+            </li>
+
+            <li>
+              <a href="#">
+                <svg
+                  style={{ height: "32px" }}
+                  className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc"
+                  focusable="false"
+                  aria-hidden="true"
+                  viewBox="0 0 24 24"
+                  data-testid="AccountCircleOutlinedIcon"
+                >
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.51.88 4.93 1.78C15.57 19.36 13.86 20 12 20s-3.57-.64-4.93-1.72zm11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6zm0 5c-.83 0-1.5-.67-1.5-1.5S11.17 8 12 8s1.5.67 1.5 1.5S12.83 11 12 11z" />
+                </svg>
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                <svg
+                  style={{ height: "32px" }}
+                  className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc"
+                  focusable="false"
+                  aria-hidden="true"
+                  viewBox="0 0 24 24"
+                  data-testid="AccountBalanceWalletOutlinedIcon"
+                >
+                  <path d="M21 7.28V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-2.28c.59-.35 1-.98 1-1.72V9c0-.74-.41-1.37-1-1.72zM20 9v6h-7V9h7zM5 19V5h14v2h-6c-1.1 0-2 .9-2 2v6c0 1.1.9 2 2 2h6v2H5z" />
+                  <circle cx={16} cy={12} r="1.5" />
+                </svg>
+              </a>
+            </li>
+
+            <li>
+              <button>
+                <svg
+                  className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc"
+                  focusable="false"
+                  aria-hidden="true"
+                  viewBox="0 0 24 24"
+                  data-testid="MenuOutlinedIcon"
+                >
+                  <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+                </svg>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
