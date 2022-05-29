0022449ca9aad537edc5d91a5002bd0503655055
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-05-29 12:01:40 +0600

    filter all worked

--- src/Pages/AllFilters/AllFilters.js
+++ src/Pages/AllFilters/AllFilters.js
@@ -139,8 +139,8 @@ function AllFilters(props) {
                                     <input type="number" className="p-2 border border-dark form_custom_control w-100 bg-transparent" placeholder='Max' />
                                 </div>
                                 <div className="col-5">
-                                    <input type="text" className="p-2 border border-dark form_custom_control w-100 bg-transparent"
-                                        placeholder='Apply' />
+                                    <input type="button" className="p-2 border border-dark form_custom_control w-100 bg-transparent"
+                                        value='Apply' />
                                 </div>
                             </div>
                         </div>
@@ -159,13 +159,214 @@ function AllFilters(props) {
                                 </div>
                             </div>
                         </div>
+                        <div className="filter_wrapper border border-dark">
+                            <div className="filter_item p-2">
+                                <div className="search_control border_all_side d-flex align-items-center
+                                 form_custom_control p-2 px-3 radius_10 mb-3"><i className="fas fa-search text-white me-2" /><input type="search" className="w-100 form_custom_control" placeholder="Filter" /></div>
+                            </div>
+                        </div>
+                        <div className="filter_wrapper border border-dark">
+                            <div className="filter_item p-2">
+                                <div className="collection_wrapper">
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="collection1" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="collection1">CollectionName</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="collection2" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="collection2">CollectionName 2</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="collection3" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="collection3">CollectionName 3</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="filter_wrapper border border-dark">
+                            <div className="filter_item d-flex align-items-center justify-content-between p-2">
+                                <div className="box">
+                                    Chains
+                                </div>
+                                <div className="box">
+                                    <button className='bg-transparent border-0'>
+                                        <svg height={25} fill="var(--text-white-75)" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowDropUpIcon"><path d="m7 14 5-5 5 5z" /></svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="filter_wrapper border border-dark">
+                            <div className="filter_item p-2">
+                                <div className="collection_wrapper">
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2" >
+                                        <input type="checkbox" className="custom-control-input" id="Ethereum" defaultChecked={true} />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Ethereum">Ethereum</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="Polygon" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Polygon">Polygon</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="Klaytn" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Klaytn">Klaytn</label>
+                                    </div>
+
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="Solona" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Solona">Solona</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="filter_wrapper border border-dark">
+                            <div className="filter_item d-flex align-items-center justify-content-between p-2">
+                                <div className="box">
+                                    Categories
+                                </div>
+                                <div className="box">
+                                    <button className='bg-transparent border-0'>
+                                        <svg height={25} fill="var(--text-white-75)" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowDropUpIcon"><path d="m7 14 5-5 5 5z" /></svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="filter_wrapper border border-dark">
+                            <div className="filter_item p-2">
+                                <div className="collection_wrapper">
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2" >
+                                        <input type="checkbox" className="custom-control-input" id="Art" defaultChecked={true} />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Art">Art</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="Collectibles" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Collectibles">Collectibles</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="Domain" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Domain">Domain</label>
+                                    </div>
+
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="Music" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Music">Music</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="Photography" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Photography">Photography</label>
+                                    </div>
+
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="Sports" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="Sports">Sports</label>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                        <div className="filter_wrapper border border-dark">
+                            <div className="filter_item d-flex align-items-center justify-content-between p-2">
+                                <div className="box">
+                                    On Sale in
+                                </div>
+                                <div className="box">
+                                    <button className='bg-transparent border-0'>
+                                        <svg height={25} fill="var(--text-white-75)" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowDropUpIcon"><path d="m7 14 5-5 5 5z" /></svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="filter_wrapper border border-dark">
+                            <div className="filter_item p-2">
+
+                                <div className="">
+                                    <div className="filter_item p-2">
+                                        <div className="search_control border_all_side d-flex align-items-center
+                                 form_custom_control p-2 px-3 radius_10 mb-3"><i className="fas fa-search text-white me-2" /><input type="search" className="w-100 form_custom_control" placeholder="Filter" /></div>
+                                    </div>
+                                </div>
+
+                                <div className="collection_wrapper">
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2" >
+                                        <input type="checkbox" className="custom-control-input" id="ETH" defaultChecked={true} />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="ETH">ETH</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="WETH" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="WETH">WETH</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="$BASED" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="$BASED">$BASED</label>
+                                    </div>
+
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="$DG" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="$DG">$DG</label>
+                                    </div>
+
+                                    <div className="custom-control custom-checkbox custom_round_checkbox mb-2">
+                                        <input type="checkbox" className="custom-control-input" id="0xBTC" />
+                                        <i className="fa-solid fa-check"></i>
+                                        <label className="custom-control-label" htmlFor="0xBTC">0xBTC</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
 
 
 
 
 
                     </div>
-                    <div className="col-sm-6 col-lg-4 col-xxl-3"></div>
                 </div>
             </div>
         </div>
